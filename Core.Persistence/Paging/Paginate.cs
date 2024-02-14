390bf0dc22e088aede3f596408fdb05fe5b6cbea
Ilker Keklik <ilkerkeklik@marun.edu.tr>
2024-02-15 00:30:52 +0300

    Paging and Dynamic filter and sort mechanisms added. Also abstract async and sync repository implementations added.

---
+++ Core.Persistence/Paging/Paginate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging;
+
+public class Paginate<T>
+{
+
+
+
+    public Paginate()
+    {
+        Items = Array.Empty<T>();
+        
+    }
+
+    public int Size { get; set; }  // # of elements in a page
+    public int Index { get; set; } // Page index
+    public int Count { get; set; } // # of elements
+    public int Pages { get; set; } // # of pages
+    public IList<T> Items { get; set; }
+    public bool HasPrevious => Index > 0;
+    public bool HasNext => Index +1 < Pages;
+
+}
