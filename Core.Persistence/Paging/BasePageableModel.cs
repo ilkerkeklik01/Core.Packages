ce3b0561b267710ef417523987b9931dae9d6759
Ilker Keklik <ilkerkeklik@marun.edu.tr>
2024-02-20 01:47:48 +0300

    PageRequest and GetListResponse inherited from BasePageableModel are implemented

---
+++ Core.Persistence/Paging/BasePageableModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging;
+
+public abstract class BasePageableModel
+{
+    public int Size { get; set; }  // # of elements in a page
+    public int Index { get; set; } // Page index
+    public int Count { get; set; } // # of elements
+    public int Pages { get; set; } // # of pages
+
+    public bool HasPrevious { get; set; }
+    public bool HasNext { get; set; }
+}
+
