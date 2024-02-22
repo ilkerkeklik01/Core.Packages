8a5ce84185e7d3b0dedfe592316579667819af8d
Ilker Keklik <ilkerkeklik@marun.edu.tr>
2024-02-22 22:43:39 +0300

    BaseBusinessrules and BusinessException added

---
+++ Core.CrossCuttingConcerns/Exceptions/Types/BusinessException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Types;
+
+public class BusinessException:Exception
+{
+
+    public BusinessException()
+    {
+        
+    }
+
+    public BusinessException(string? message):base(message)
+    {
+        
+    }
+
+    public BusinessException(string? message,Exception? innerExceptiion) : base(message, innerExceptiion)
+    {
+
+    }
+}
