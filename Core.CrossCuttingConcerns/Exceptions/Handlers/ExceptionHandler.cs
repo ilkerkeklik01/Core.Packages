a7a96ece5eb46f5dfcae69e97615bdefee22a737
İlker Keklik <94231073+ilkerkeklik01@users.noreply.github.com>
2024-12-27 03:36:16 +0300

    1.0.6 HttpExceptionHandler.cs extended for custom AuthorizationException.cs

--- Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -14,11 +14,13 @@ public abstract class ExceptionHandler
         {
             BusinessException businessException => HandleException(businessException),
             ValidationException validationException => HandleException(validationException),
+            AuthorizationException authorizationException => HandleException(authorizationException),
             _ => HandleException(exception)
         };
 
     protected abstract Task HandleException(BusinessException businessException);
     protected abstract Task HandleException(ValidationException validationException);
+    protected abstract Task HandleException(AuthorizationException authorizationException);
     protected abstract Task HandleException(Exception exception);
 
 }
--- Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
@@ -27,6 +27,13 @@ public class HttpExceptionHandler : ExceptionHandler
         return Response.WriteAsync(details);
     }
 
+    protected override Task HandleException(AuthorizationException authorizationException)
+    {
+        Response.StatusCode = StatusCodes.Status401Unauthorized;
+        string details = new InternalServerErrorProblemDetails(authorizationException.Message).AsJson();
+        return Response.WriteAsync(details);
+    }
+
     protected override Task HandleException(Exception exception)
     {
         Response.StatusCode = StatusCodes.Status500InternalServerError;
